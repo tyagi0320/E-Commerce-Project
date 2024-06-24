63d66e9f2045bf3a7ffadd266e5bb302d86dcbd3
Harshit Tyagi <tharshit03@gmail.com>
2024-06-24 17:26:04 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import Home from './pages/home/Home';
+import Cart from './pages/cart/Cart';
+import Order from './pages/order/Order';
+import Dashboard from './pages/admin/dashboard/Dashboard';
+import NoPage from './pages/nopage/NoPage';
 
 function App() {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/order" element={<Order/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/*" element={<NoPage/>} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../../components/layout/Layout'
+
+
+function Home() {
+  return (
+    <Layout>Home</Layout>
+  )
+}
+
+export default Home
