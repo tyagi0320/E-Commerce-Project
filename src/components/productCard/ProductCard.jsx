baac45151ca914a03aca08e9e14f0c41fa62207a
Harshit Tyagi <tharshit03@gmail.com>
2024-06-27 17:34:39 +0530

    update

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -3,7 +3,7 @@ import myContext from '../../context/data/myContext'
 
 function ProductCard() {
     const context = useContext(myContext)
-    const { mode } = context
+    const { mode,product } = context
     return (
         <section className="text-gray-600 body-font">
             <div className="container px-5 py-8 md:py-16 mx-auto">
