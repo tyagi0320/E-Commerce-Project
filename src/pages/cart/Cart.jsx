e9120fe4cdde33609b52c182abb5b5472f76d61b
Harshit Tyagi <tharshit03@gmail.com>
2024-06-28 00:09:00 +0530

    update

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -5,6 +5,9 @@ import Modal from "../../components/modal/Modal";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteFromCart } from "../../redux/cartSlice";
 import { toast } from "react-toastify";
+import { addDoc, collection } from "firebase/firestore";
+import { fireDB } from "../../firebase/FirebaseConfig";
+
 function Cart() {
   const context = useContext(myContext);
   const { mode } = context;
@@ -13,32 +16,108 @@ function Cart() {
   const cartItems = useSelector((state) => state.cart);
   console.log(cartItems);
 
-  const deleteCart = (item) =>{
+  const deleteCart = (item) => {
     dispatch(deleteFromCart(item));
-    toast.success("Product removed from cart",{
+    toast.success("Product removed from cart", {
       position: "bottom-right",
-    autoClose: 1500,
+      autoClose: 1500,
     });
   };
 
-  useEffect(()=>{
-    localStorage.setItem('cart',JSON.stringify(cartItems));
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cartItems));
   }, [cartItems]);
 
-
-  const [totalAmount,setTotalAmount] = useState(0);
-  useEffect(()=>{
-    let temp=0;
-    cartItems.forEach((cartItem)=>{
-      temp+=parseInt(cartItem.price)
-    })
+  const [totalAmount, setTotalAmount] = useState(0);
+  useEffect(() => {
+    let temp = 0;
+    cartItems.forEach((cartItem) => {
+      temp += parseInt(cartItem.price);
+    });
     setTotalAmount(temp);
-  },[cartItems])
+  }, [cartItems]);
 
   const shipping = parseInt(100);
-  const grandTotal = shipping+totalAmount
+  const grandTotal = shipping + totalAmount;
 
-  
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [pincode, setPincode] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const buyNow = async () => {
+    // validation
+    if (name === "" || address === "" || pincode === "" || phoneNumber === "") {
+      return toast.error("All fields are required", {
+        position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    }
+
+    const addressInfo = {
+      name,
+      address,
+      pincode,
+      phoneNumber,
+      date: new Date().toLocaleString("en-US", {
+        month: "short",
+        day: "2-digit",
+        year: "numeric",
+      }),
+    };
+
+    var options = {
+      key: "",
+      key_secret: "",
+      amount: parseInt(grandTotal * 100),
+      currency: "INR",
+      order_receipt: "order_rcptid_" + name,
+      name: "Shopline",
+      description: "for testing purpose",
+      handler: function (response) {
+        console.log(response);
+        toast.success("Payment Successful");
+
+        const paymentId = response.razorpay_payment_id;
+
+        //store in firebase
+        const orderInfo = {
+          cartItems,
+          addressInfo,
+          date: new Date().toLocaleString("en-US", {
+            month: "short",
+            day: "2-digit",
+            year: "numeric",
+          }),
+          email: JSON.parse(localStorage.getItem("user")).user.email,
+          userid: JSON.parse(localStorage.getItem("user")).user.uid,
+          paymentId,
+        };
+        try {
+          const result = addDoc(collection(fireDB, "orders"), orderInfo);
+          /*const orderRef= collection(fireDB,'order')
+         addDoc(orderRef, orderInfo)*/
+        } catch (error) {
+          console.error(error);
+        }
+      },
+
+      theme: {
+        color: "#3399cc",
+      },
+    };
+
+    var pay = new window.Razorpay(options);
+    pay.open();
+    console.log(pay);
+  };
+
   return (
     <Layout>
       <div
@@ -52,7 +131,7 @@ function Cart() {
         <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0 ">
           <div className="rounded-lg md:w-2/3 ">
             {cartItems.map((item, index) => {
-              const {title,price,description,imageUrl} =item;
+              const { title, price, description, imageUrl } = item;
               return (
                 <div
                   className="justify-between mb-6 rounded-lg border  drop-shadow-xl bg-white p-6  sm:flex  sm:justify-start"
@@ -87,9 +166,10 @@ function Cart() {
                         ₹{price}
                       </p>
                     </div>
-                    <div 
-                    onClick={()=>deleteCart(item)}
-                    className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+                    <div
+                      onClick={() => deleteCart(item)}
+                      className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6"
+                    >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -159,12 +239,22 @@ function Cart() {
                   className="mb-1 text-lg font-bold"
                   style={{ color: mode === "dark" ? "white" : "" }}
                 >
-                 {grandTotal}
+                  {grandTotal}
                 </p>
               </div>
             </div>
             {/* <Modal  /> */}
-            <Modal />
+            <Modal
+              name={name}
+              address={address}
+              pincode={pincode}
+              phoneNumber={phoneNumber}
+              setName={setName}
+              setAddress={setAddress}
+              setPincode={setPincode}
+              setPhoneNumber={setPhoneNumber}
+              buyNow={buyNow}
+            />
           </div>
         </div>
       </div>
